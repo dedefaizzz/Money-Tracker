2b7c1cedffa0b35ed50f1c105cf1773e16a51bf1
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2024-01-08 20:40:45 +0700

    penjelasan dashboard

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -6,25 +6,34 @@ import 'package:money_tracker/widgets/navbar.dart';
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
 
+  // objek state dashboard
   @override
   State<Dashboard> createState() => _DashboardState();
 }
 
 class _DashboardState extends State<Dashboard> {
+  // Variabel untuk menandakan status loading saat logout.
   var isLogoutLoading = false;
+
+  // Variabel untuk menyimpan indeks halaman yang sedang aktif di bottom navigation.
   int currentIndex = 0;
+
+  // List halaman yang akan ditampilkan di dalam PageView.
   var pageViewList = [HomeScreen(), TransactionScreen()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: NavBar(
+          // Mengatur indeks yang dipilih berdasarkan currentIndex
           selectedIndex: currentIndex,
+          // Callback saat destinasi dipilih untuk mengubah currentIndex
           onDestinatioSelected: (int value) {
             setState(() {
               currentIndex = value;
             });
           }),
+      // Menampilkan halaman yang sesuai dengan currentIndex
       body: pageViewList[currentIndex],
     );
   }
