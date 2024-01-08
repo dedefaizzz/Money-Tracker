1220088a693921fb36320250c4ff5a4a69adf0fd
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2024-01-08 21:56:43 +0700

    penjelasan auth_services.dart

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -5,17 +5,23 @@ import 'package:money_tracker/services/db.dart';
 
 class AuthService {
   var db = Db();
+  // Metode createUser untuk membuat akun baru berdasarkan data yang diberikan
   createUser(data, context) async {
     try {
+      // Membuat user baru menggunakan email dan password
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: data['email'],
         password: data['password'],
       );
+
+      // menambah data user ke database
       await db.addUser(data, context);
+      // navigasi ke dashboard setelah berhasil dibuat
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => Dashboard()),
       );
     } catch (e) {
+      // error jika salah saat membuat akun
       showDialog(
           context: context,
           builder: (context) {
@@ -27,6 +33,7 @@ class AuthService {
     }
   }
 
+  // method login
   login(data, context) async {
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
