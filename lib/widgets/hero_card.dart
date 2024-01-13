28f91fabb4bc62892d202fa6d88ab2121360290d
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2024-01-13 07:37:37 +0700

    penjelasan hero card

--- lib/widgets/hero_card.dart
+++ lib/widgets/hero_card.dart
@@ -1,35 +1,42 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+// class herocard untuk menampilkan informasi saldo pengguna
 class HeroCard extends StatelessWidget {
+  // parameter key dan userId
   HeroCard({
     super.key,
     required this.userId,
   });
-  final String userId;
+  final String userId; // menyimpan id pengguna
 
   @override
   Widget build(BuildContext context) {
+    // stream yg melakukan perubahan dokumen di firestore
     final Stream<DocumentSnapshot> _usersStream =
         FirebaseFirestore.instance.collection('users').doc(userId).snapshots();
 
+    // build UI berdasarkan snapshot dari stream
     return StreamBuilder<DocumentSnapshot>(
       stream: _usersStream,
       builder:
           (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
         if (snapshot.hasError) {
-          return Text('Something went wrong');
+          return Text('Something went wrong'); // jika terjadi kesalahan
         }
 
         if (!snapshot.hasData || !snapshot.data!.exists) {
-          return const Text('Document does not exist');
+          return const Text(
+              'Document does not exist'); // jika tidak ada dokumen
         }
 
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
+          return Text("Loading"); // stream menunggu data
         }
+        // mendapatkan data dari snapshot
         var data = snapshot.data!.data() as Map<String, dynamic>;
 
+        // widget cards dgn data yg disimpan
         return Cards(
           data: data,
         );
@@ -38,12 +45,13 @@ class HeroCard extends StatelessWidget {
   }
 }
 
+// menampilkan saldo & statistik pengguna
 class Cards extends StatelessWidget {
   const Cards({
     super.key,
     required this.data,
   });
-  final Map data;
+  final Map data; // menyimpan data pengguna
 
   @override
   Widget build(BuildContext context) {
@@ -51,6 +59,7 @@ class Cards extends StatelessWidget {
       color: Colors.deepPurple,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
+        // bagian saldo pengguna
         children: [
           Padding(
             padding: const EdgeInsets.all(15.0),
@@ -77,6 +86,7 @@ class Cards extends StatelessWidget {
             ),
           ),
           SizedBox(height: 10),
+          // UI pemasukan & pengeluaran
           Column(
             children: [
               Container(
@@ -87,6 +97,7 @@ class Cards extends StatelessWidget {
                     borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(20),
                         topRight: Radius.circular(20))),
+                // bagian pemasukan & pengeluaran
                 child: Row(
                   children: [
                     CardOne(
@@ -111,6 +122,7 @@ class Cards extends StatelessWidget {
   }
 }
 
+// menampilkan pendapatan / pengeluaran
 class CardOne extends StatelessWidget {
   const CardOne({
     super.key,
@@ -119,9 +131,9 @@ class CardOne extends StatelessWidget {
     required this.amount,
   });
 
-  final Color color;
-  final String heading;
-  final String amount;
+  final Color color; // menyimpan background
+  final String heading; // menyimpan judul kartu (income / expense)
+  final String amount; // menyimpan jml income / expense
 
   @override
   Widget build(BuildContext context) {
@@ -132,6 +144,7 @@ class CardOne extends StatelessWidget {
             borderRadius: BorderRadius.circular(10)),
         child: Row(
           children: [
+            // bagian field kiri (judul dan jumlah)
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
@@ -152,6 +165,7 @@ class CardOne extends StatelessWidget {
               ),
             ),
             Spacer(),
+            // field kanan icon income / expense
             Padding(
               padding: const EdgeInsets.all(10.0),
               child: Icon(
